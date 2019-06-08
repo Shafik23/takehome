98d6ddab31ecae4a520d116c2759a443d328f052
Shafik <shafik23@gmail.com>
2019-06-08 14:40:21 -0700

    Added more tests, and wrapped up REFLECTION.md

--- test/test-api.js
+++ test/test-api.js
@@ -22,9 +22,15 @@ it('Response format', function(done) {
     this.timeout(10000);
     request('http://localhost:3000' , function(error, response, body) {
         const bodyObject = JSON.parse(body);
+
         expect(bodyObject).to.have.property('twitter');
         expect(bodyObject).to.have.property('facebook');
         expect(bodyObject).to.have.property('instagram');
+
+        expect(bodyObject.twitter).to.be.an('array');
+        expect(bodyObject.facebook).to.be.an('array');
+        expect(bodyObject.instagram).to.be.an('array');
+
         done();
     });
 });
